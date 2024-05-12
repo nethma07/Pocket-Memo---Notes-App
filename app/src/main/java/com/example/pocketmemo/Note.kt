ceaf8b3a28552a0ba77f393ac67679a15c59c32b
nipuni01 <nipunigunawardana777@gmail.com>
2024-05-12 21:35:38 +0530

    creating the project

---
+++ app/src/main/java/com/example/pocketmemo/Note.kt
@@ -0,0 +1,3 @@
+package com.example.pocketmemo
+
+data class Note(val id: Int, val title: String, val content: String)
---
+++ app/src/main/java/com/example/pocketmemo/UpdateNoteActivity.kt
@@ -0,0 +1,50 @@
+package com.example.pocketmemo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pocketmemo.databinding.ActivityUpdateNoteBinding
+
+class UpdateNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityUpdateNoteBinding
+    private lateinit var db: NoteDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1){
+            finish()
+            return
+        }
+
+        val note = db.getNoteByID(noteId)
+        binding.updateTitleEditText.setText(note.title)
+        binding.updateContentEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener {
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updatedNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updatedNote)
+            finish()
+            Toast.makeText(this, "Changes Saved", Toast.LENGTH_SHORT).show()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
